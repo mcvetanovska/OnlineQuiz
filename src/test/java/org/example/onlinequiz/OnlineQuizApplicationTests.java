6b97cd0a92e09c3f7d3e6af3ab4e7883c1ae0692
mcvetanovska <mcvetanovska@hotmail.com>
2024-06-20 12:47:22 +0200

    initial commit

---
+++ src/test/java/org/example/onlinequiz/OnlineQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
